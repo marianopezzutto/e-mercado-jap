ba6045f5e348ded92edee60d73412e0fe37b8186
marianopezzutto <marianopezzutto.mp@gmail.com>
2022-08-21 16:15:27 -0300

    js of google buttom

--- js/login.js
+++ js/login.js
@@ -12,6 +12,15 @@ document.addEventListener("DOMContentLoaded", function(){
         let password = document.getElementById("password").value;
         loginRequired(email, password);
     });
+
+    document.getElementById("singin").addEventListener("click", function(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+          
+    });
 });
     
 
