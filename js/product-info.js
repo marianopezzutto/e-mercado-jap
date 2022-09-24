2f03c4c12d3fa905992667afe70b76705e9c84b0
marianopezzutto <marianopezzutto.mp@gmail.com>
2022-09-23 23:09:11 -0300

    related products

--- js/product-info.js
+++ js/product-info.js
@@ -3,42 +3,62 @@ let info_product_url = PRODUCT_INFO_URL + id_product + EXT_TYPE;
 let coment_product = PRODUCT_INFO_COMMENTS_URL + id_product + EXT_TYPE;
 let com = {};
 
+function setProID(id) {
+    localStorage.setItem("proID", id);
+    window.location = "product-info.html"
+}
 
-/* Function that show the information of the products */
+/* ------------------ Function that show information of the products. ------------------ */
 
 function showInfoProduct(infopro) {
     
    let htmlContentToAppend = 
-   `
-    <div class="card mt-3">
-        <div class="card-body">
-            <h1 class="card-title">${infopro.name}</h1>
-            <p class=""><strong>Precio:</strong> ${infopro.cost}${infopro.currency}</p>
-            <p class=""><strong>Descripción:</strong> ${infopro.description}</p>
-            <p class=""><strong>Categoria:</strong> ${infopro.category}</p>
-            <p class=""><strong>Cantidad vendida:</strong> ${infopro.soldCount}</p>
-            <p class=""><strong>Imágenes ilustrativas:</strong></p>
-        </div>
-        <div class="row">
-    `
-    for (let i = 0; i < infopro.images.length; i++) {
-        const element = infopro.images[i];
-        
-        htmlContentToAppend += 
-        `<div class="col-md-3 mb-3">
-            <img class="bd-placeholder-img card-img-top img-thumbnail" src="${element}">
-        </div>
-        ` 
-    }
-    htmlContentToAppend +=
-        `    
+   `<div class="card mt-3 alert-secondary">
+        <div class="row align-items-start">
+      
+            <div class="col-8">
+                <div id="carouselExampleControls" class="carousel slide carousel-dark" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="${infopro.images[0]}" class="d-block w-100" alt="...">
+                        </div>`;
+                        for (let i = 1; i < infopro.images.length; i++) {
+                            const element = infopro.images[i];
+                            
+                            htmlContentToAppend += 
+                        `<div class="carousel-item">
+                            <img src="${element}" class="d-block w-100" alt="...">
+                        </div>`;
+                        }
+                            htmlContentToAppend +=
+                    `</div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
-        </div>
+                  
+            <div class="col-4">
+                <h1 class="">${infopro.name}</h1>
+                <p class=""><strong>Precio:</strong> ${infopro.cost}${infopro.currency}</p>
+                <p class=""><strong>Descripción:</strong> ${infopro.description}</p>
+                <p class=""><strong>Categoria:</strong> ${infopro.category}</p>
+                <p class=""><strong>Cantidad vendida:</strong> ${infopro.soldCount}</p>
+            </div>
+
+        </div>    
+    </div>
+    
         <div class="d-grid gap-2 mt-4 mb-4">
             <button class="btn btn-lg btn-primary" type="button">Agregar al carrito</button>
         </div>
         <div class="container card">
-            <div class="row align-items-start pb-3" id="">
+            <div class="row align-items-start pb-3 alert-secondary" id="">
                 <div class="col-md-6" >
                     <h3 class="mt-3">Comentarios</h3>
                     <div id="commentContainer" style="height:400px; overflow: scroll;"></div>
@@ -46,13 +66,31 @@ function showInfoProduct(infopro) {
                 <div class="col-md-6" id="inputContainer">
                     <h3 class="mt-3">Dejar un comentario:</h3>
                 </div>
-        `;
+            </div>
+        </div>
+        <div class="container card mt-3 alert-secondary">
+            <div class="row align-items-start pb-3" id="">       
+                <h3 class="mt-3">Productos relacionados</h3>`
+                for (let i = 0; i < infopro.relatedProducts.length; i++) {
+                    const related_products = infopro.relatedProducts[i];
+                    htmlContentToAppend +=
+                    `
+                    <div class="col">
+                      <div onclick="setProID(${related_products.id})" class="card cursor-active">
+                        <div class="card-body">
+                        <img class="bd-placeholder-img card-img-top img-thumbnail" src="${related_products.image}">
+                          <p class="card-text">${related_products.name}</p>
+                        </div>
+                      </div>
+                    </div>
+                    `
+                }
 
     document.getElementById("infoContainer").innerHTML = htmlContentToAppend;
 }
 
 
-/* Function that show the comments of each product */
+/* ------------------ Function that show comments of each product. ------------------ */
 
 function showComment(comentarios) {
     
@@ -77,7 +115,7 @@ function showComment(comentarios) {
 }
 
 
-/* Function that show the posibility to send a comment */
+/* ------------------ Function that show the posibility to send a comment. ------------------ */
 
 function showInputComment(){
     let htmlInputToAppend ="";
@@ -102,7 +140,7 @@ function showInputComment(){
 }
 
 
-/* function to show the rating stars of each comment */
+/* ------------------ Function to show the rating stars of each comment. ------------------*/
 
 function showScore(numero) {
     let stars = `<span class="fa fa-star checked"></span>`;
@@ -133,6 +171,8 @@ function mostrar(){
     }
     
 }
+
+
 
 
 
